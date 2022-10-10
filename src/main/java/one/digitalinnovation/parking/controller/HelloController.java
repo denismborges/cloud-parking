b510a786de61c108ed6d51cb4b1ce82216af8d76
denis.borges <denis.borges@gmail.com>
2022-10-10 16:05:16 -0300

    teste de pipeline no heroku

--- src/main/java/one/digitalinnovation/parking/controller/HelloController.java
+++ src/main/java/one/digitalinnovation/parking/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @GetMapping
     public String hello() {
-	return "Hello World. Java Devs.";
+	return "Hello World. Java Devs. Teste heroku";
     }
     
 }
